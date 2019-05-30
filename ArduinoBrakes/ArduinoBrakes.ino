63060f2214200e3e6d1473544c22b1b213e08875
Tristan Hess <tlh173@zips.uakron.edu>
2019-05-30 15:08:26 -0400

    Rewrite setup and implement loops

--- ArduinoBrakes/ArduinoBrakes.ino
+++ ArduinoBrakes/ArduinoBrakes.ino
@@ -1,26 +1,35 @@
 void setup() {
   // put your setup code here, to run once:
+  /* import library to read from altimeter here 
+   * write altimeter setup here
+   * import library for component that activates airbrakes
+   * write airbrakes component setup here
+   */
 
+  Serial.begin(9600);
+}
+
+void loop() {
+  
   // Establishing Known Values
   float g = 32.17;
-  int hi = 0;
+  float hi = 0;
   float vi = 0;
   float A = 0.25;
-  int Cdo = 1;
+  float Cdo = 1;
   float Cdc = 0.75;
   float t = 0.1;
-  int h = 0;
-  int hf = 0;
-  int v = 0;
+  float h = 0;
+  float hf = 0;
+  float v = 0;
+  float p = 0.0;
 
   // Establishing Input Values
   /* This line is done to read input from the user. 
    * If the values for the mass and desired apogee can be coded beforehand,
    * it would make this program a lot more simple.
    */ 
-
-  Serial.begin(9600);
-
+   
   Serial.print("Input mass of rocket in slugs");
   while (Serial.available() == 0) {}
   float m = Serial.read();
@@ -34,9 +43,59 @@ void setup() {
   // Finding the Values
   float vo = (d * 2) / (t - vi);
   vi = vo;
-}
+  
+  // until the final height of the rocket is above the desired apogee,
+  // this loop will continue to run
+  while (hf < ho) {
+    
+    hi = h;
+    Serial.print("Input current height of the rocket");
+    while (Serial.available() == 0) {}
+    h = Serial.read();
+    vi = v;
+    d = ho - h;
+    
+    if (h < 5000)
+      p = 23.77 * 0.0001;
+      
+    if (h < 10000)
+      p = 20.48 * 0.0001;
+      
+    if (h >= 10000)
+      p = 17.56 * 0.0001;
 
-void loop() {
-  // put your main code here, to run repeatedly:
+    float visq = vi * vi;
+    hf = (0.5 * visq - d * Cdc * p * visq * A / 2) / (m * g);
+    v = (h - hi) / t;
+    
+  }
 
+  Serial.print("Activate second loop");
+
+  while (hf <= ho) {
+    hi = h;
+    Serial.print("Input current height of the rocket");
+    while (Serial.available() == 0) {}
+    h = Serial.read();
+    vi = v;
+    d = ho - h;
+
+    if (h < 5000)
+      p = 23.77 * 0.0001;
+      
+    if (h < 10000)
+      p = 20.48 * 0.0001;
+      
+    if (h >= 10000)
+      p = 17.56 * 0.0001;
+
+    float visq = vi * vi;
+    hf = (0.5 * m * visq - d * Cdo * p * visq * A / 2) / (m * g);
+    v = (h - hi) / t;
+  }
+
+  Serial.print("ACTIVATE AIRBRAKES");
+
+  // function call or another loop to activate airbrakes
+  
 }
